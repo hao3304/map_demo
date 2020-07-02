3a7bda9739b4bfe581542313e8bc2b628d7319e4
jack <hao3304@qq.com>
2020-07-02 22:40:27 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="dashboard">
+    <header></header>
+    <div class="content">
+      <div class="block"></div>
+      <div id="map" ref="map">
+        <div class="left">
+          <div class="box">
+            <left1></left1>
+          </div>
+        </div>
+        <div class="right"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import L from "leaflet";
+import "leaflet.chinatmsproviders";
+import left1 from "./left1";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: { left1 },
+  methods: {
+    renderMap() {
+      this.map = new L.map(this.$refs.map, {
+        zoom: 18,
+        center: [30.75346296449824, 120.74784636497498],
+        attributionControl: false,
+        zoomControl: false
+      });
+      L.tileLayer.chinaProvider("Google.Satellite.Map").addTo(this.map);
+
+      this.map.on("click", e => {
+        console.log(e);
+      });
+    }
+  },
+  mounted() {
+    this.renderMap();
   }
 };
 </script>
+<style lang="less">
+.dashboard {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  header {
+    height: 80px;
+    background-image: url("~@/assets/header.png");
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
+  }
+  .content {
+    flex: 1;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    #map {
+      flex: 1;
+      position: relative;
+
+      .left {
+        width: 600px;
+        background: linear-gradient(
+          to right,
+          rgba(0, 0, 0, 0.8),
+          rgba(0, 0, 0, 0)
+        );
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        z-index: 1000;
+        .box {
+          width: 400px;
+          position: absolute;
+          left: 0;
+          bottom: 40px;
+          top: 20px;
+          border-top: 1px solid rgb(0, 232, 70);
+          border-right: 1px solid rgb(0, 232, 70);
+          border-bottom: 1px solid rgb(0, 232, 70);
+        }
+      }
+
+      .right {
+        width: 600px;
+        background: linear-gradient(
+          to left,
+          rgba(0, 0, 0, 0.8),
+          rgba(0, 0, 0, 0)
+        );
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 1000;
+      }
+    }
+    .block {
+      height: 64px;
+    }
+  }
+}
+</style>
---
+++ src/views/left1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-left">
+    <div class="item">
+      <div class="item-header"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "left1"
+};
+</script>
+
+<style scoped></style>
